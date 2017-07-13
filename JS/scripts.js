8c0ed739677e614a4627b44624f0b8b71adb126b
albert <albert.cymbaluk@gmail.com>
2017-07-13 21:16:24 +0200

    send proj

---
+++ JS/scripts.js
@@ -0,0 +1,89 @@
+var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
+var dinosaur = 'triceratops';
+var dinosaurUpperCased = dinosaur.toUpperCase();
+
+var textCharsAfter = text.replace('Velociraptor' , dinosaurUpperCased );
+
+
+console.log(textCharsAfter.slice(0, 72));
+
+
+
+
+
+/*
+var a = prompt ('Podaj wartość a');
+var h = prompt ('Podaj wartość h');
+var zadanie = (a*h/2);
+
+
+function getTriangleArea(a, h) {
+
+if ( (a <= 0) || (h <= 0)) {
+    console.log('dane nieprawidłowe');
+} else {
+  return (a*h/2);
+}
+}
+var triangle1Area = getTriangleArea (a, h);
+  console.log(triangle1Area);
+*/
+
+
+
+
+// nowe komantarze są w modzie
+ /* var name = prompt('Enter your name');
+  alert('Hello, ' + name);
+ console.log('Hello, ' + name);
+*/
+/*
+var a = prompt ('Podaj wartość a');
+var  h = prompt ('Podaj wartość h');
+var triangleArea = a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+alert('Hello, ' + triangleArea);
+*/
+
+/*
+var a = prompt ('Podaj wartość a');
+var  b = prompt ('Podaj wartość b');
+var wynik = (a * a) + (2 * a * b) - (b * b);
+console.log(wynik);
+var value = wynik;
+
+if ( value > 0) {
+    console.log('wynik dodatni');
+} else if (value < 0 ) {
+    console.log('wynik ujemny');
+}
+if ( value == 0) {
+   console.log('jest rowny 0');
+}
+*/
+
+
+/*
+var a = prompt ('Podaj wartość a');
+var h = prompt ('Podaj wartość h');
+var zadanie = (a*h/2);
+
+    function getTriangleArea (a, h) {
+
+	if ( (a <= 0) || (h <= 0) ) {
+	console.log('dane nieprawidłowe');
+} else {
+   return zadanie;
+}
+
+var winner = checkResult (userChecked, computerChecked);
+
+
+
+var a = prompt ('Podaj wartość a');
+var h = prompt ('Podaj wartość h');
+
+if ( (a <= 0) || (h <= 0)) {
+    console.log('dane nieprawidłowe');
+} 
+*/
